03ab98fe5b24c04aac8192ee28d84c531e60e90d
pokC01 <pokC01@x.y.z>
2023-05-31 11:54:11 +0200

    editando js

--- Practica4.5/ejercicio4.5.js
+++ Practica4.5/ejercicio4.5.js
@@ -1,1 +1,18 @@
-fetch
+function myAnimeLyst() {
+    fetch('https://api.jikan.moe/v4/random/anime')
+    .then(respone => respone.json())
+    .then(dato=>{
+        dato => dato.data
+
+        //titulos de los animes
+        const titulo = dato.title;
+        const tituloJapo = dato.title_japanese;
+        const tituloIng  = dato.title_english;
+
+
+
+    })
+    .catch(error =>{
+        console.error("Error no se ha podido cargar la pagina", error)
+    })
+}
